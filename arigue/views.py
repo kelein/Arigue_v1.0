a1744371520ce38fac99ee6fa5d0dc01e2fc762c
Kallen Ding <keleix@gmail.com>
2015-08-28 18:04:13 +0800

    Implent pagination in server.vew and do some exception solution occurred by invliad page index.

--- arigue/views.py
+++ arigue/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template import loader
 from django.contrib import auth
 
+from django.core.paginator import Paginator
+
 from arigue.models import *
 # Create your views here.
 
@@ -45,5 +47,28 @@ def dashboard(request):
 # Server manager view
 def server(request):
   	t = loader.get_template('server.html')
-	servers = Server.objects.all()
-	return render_to_response('server.html', {'servers': servers})
+	# Get all objects in model Server
+	allServers = Server.objects.all()
+	# Records of each page
+	pageSize = 5
+	# Instance a Paginator object 
+	paginator = Paginator(allServers, pageSize)	
+	# Total page number
+	pageCount = paginator.num_pages
+	print "Total Page: %s" % pageCount
+
+	# ## Exception Solutino ##	
+	try:
+  		pageIndex = int(request.GET.get('page', '1'))
+		print "Page Index: %s" % pageIndex
+	except ValueError:
+		pageIndex = 1
+		perPage = paginator.page(pageIndex)	
+	# Raised when page() is given a valid value but 
+	# no objects exist on that page.
+	try:
+		perPage = paginator.page(pageIndex)	
+	except (EmptyPage, InvaildPage):
+		perPage = paginator.page(pageCount)	
+	
+	return render_to_response('server.html', {'perPage': perPage})
